7651573f89e5c0782f7c8fa0dd7efb2ab1bd6dc9
Z.M.Lee <zmlee1001@icloud.com>
2025-07-19 17:26:35 +0800

    first git for yellow board detection

---
+++ build/CMakeFiles/opencv.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/VScode_Projects/board_detection/src/board_detector.cpp" "CMakeFiles/opencv.dir/src/board_detector.cpp.obj" "gcc" "CMakeFiles/opencv.dir/src/board_detector.cpp.obj.d"
+  "E:/VScode_Projects/board_detection/src/cam_capture.cpp" "CMakeFiles/opencv.dir/src/cam_capture.cpp.obj" "gcc" "CMakeFiles/opencv.dir/src/cam_capture.cpp.obj.d"
+  "E:/VScode_Projects/board_detection/src/main.cpp" "CMakeFiles/opencv.dir/src/main.cpp.obj" "gcc" "CMakeFiles/opencv.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/opencv.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv.dir/src/board_detector.cpp.obj"
+  "CMakeFiles/opencv.dir/src/board_detector.cpp.obj.d"
+  "CMakeFiles/opencv.dir/src/cam_capture.cpp.obj"
+  "CMakeFiles/opencv.dir/src/cam_capture.cpp.obj.d"
+  "CMakeFiles/opencv.dir/src/main.cpp.obj"
+  "CMakeFiles/opencv.dir/src/main.cpp.obj.d"
+  "libopencv.dll.a"
+  "opencv.exe"
+  "opencv.exe.manifest"
+  "opencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
